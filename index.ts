601fe756a2c9d6fa3b4d6a38c28373e238b44f70
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-10-15 10:11:59 +0700

    setup database

--- index.ts
+++ index.ts
@@ -2,9 +2,22 @@ import 'dotenv/config';
 
 import { serverConfig } from './config/server.config';
 import { Server } from './src/server/server';
+import { connect } from './src/db/connect';
 
 const server = new Server(serverConfig);
 
-server.listen((port: number) => {
-  console.log(`server listening at ${port}`);
-});
+async function run() {
+  try {
+    await connect();
+
+    server.listen((port: number) => {
+      console.log(`server listening at ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+
+    process.exit(1);
+  }
+}
+
+run();
