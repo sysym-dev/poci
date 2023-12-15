6e8fbfd0806cda01300badd0bc5e196e497095be
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-16 06:02:12 +0700

    todo get all

--- src/modules/todo/todo.model.js
+++ src/modules/todo/todo.model.js
@@ -1,18 +1,26 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../../db/sequelize.js');
 
-exports.Todo = sequelize.define('Todo', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
+exports.Todo = sequelize.define(
+  'Todo',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.ENUM,
+      values: ['todo', 'inprogress', 'done'],
+      defaultValue: 'todo',
+      allowNull: false,
+    },
   },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
+  {
+    tableName: 'todos',
+    underscored: true,
   },
-  status: {
-    type: DataTypes.ENUM,
-    values: ['todo', 'inprogress', 'done'],
-    defaultValue: 'todo',
-  },
-});
+);
