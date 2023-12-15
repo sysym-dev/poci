32ae883fcd33389ea6fab34275f866ab33a6d397
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-15 18:29:40 +0700

    database

---
+++ src/db/config.js
@@ -0,0 +1,7 @@
+exports.config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  name: process.env.DB_NAME,
+};
