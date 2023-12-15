358bd29fd34d9cd213b4b4c6f75f9497dcda3d73
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-16 06:21:32 +0700

    todo schema

--- src/modules/todo/todo.resource.js
+++ src/modules/todo/todo.resource.js
@@ -1,6 +1,11 @@
+const Joi = require('joi');
 const { Todo } = require('./todo.model.js');
 
 exports.TodoResource = class {
   url = '/todos';
   model = Todo;
+  schema = Joi.object({
+    name: Joi.string().required(),
+    description: Joi.string().optional(),
+  });
 };
