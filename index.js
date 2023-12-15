d45dc70cd5eabd7a3bd9a0ed4e5b6794ca16f9a1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-15 18:29:51 +0700

    connect db

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv/config');
+}
+
 const { createServer } = require('./src/server/create-server.js');
+const { TodoResource } = require('./src/modules/todo/todo.resource.js');
+const { sequelize } = require('./src/db/sequelize.js');
 
-const server = createServer({
-  port: 3000,
-  resources: ['todos'],
-});
+async function start() {
+  try {
+    await sequelize.authenticate();
 
-server.listen();
+    const server = createServer({
+      port: 3000,
+      resources: [TodoResource],
+    });
+
+    server.listen();
+  } catch (err) {
+    console.error(err);
+
+    process.exit(1);
+  }
+}
+
+start();
