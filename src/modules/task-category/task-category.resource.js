651ac5941095aa166479b5fa8d4822c12da9ed24
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-12-21 13:53:03 +0700

    count tasks attribute on category

--- src/modules/task-category/task-category.resource.js
+++ src/modules/task-category/task-category.resource.js
@@ -2,13 +2,37 @@ const Joi = require('joi');
 const { TaskCategory } = require('./model/task-category.model.js');
 const { Op } = require('sequelize');
 const { optionalProperty } = require('../../utils/object.js');
+const {
+  createCountRelationAttribute,
+} = require('../../resource/query/create-count-relation-attribute.js');
 
 exports.TaskCategoryResource = class {
   url = '/task-categories';
   model = TaskCategory;
 
   attributes() {
-    return ['id', 'name', 'description', 'createdAt', 'updatedAt'];
+    return [
+      'id',
+      'name',
+      'description',
+      'createdAt',
+      'updatedAt',
+      createCountRelationAttribute({
+        table: 'task_categories',
+        relation: 'tasks',
+        foreignKey: 'task_category_id',
+        as: 'tasks_count',
+      }),
+      createCountRelationAttribute({
+        table: 'task_categories',
+        relation: 'tasks',
+        foreignKey: 'task_category_id',
+        where: {
+          status: 'done',
+        },
+        as: 'tasks_done_count',
+      }),
+    ];
   }
 
   schema(options) {
