1976c7c2627fd25f4b8af3bd94102869679b9811
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-20 16:51:57 +0700

    fix: resend not unique

--- src/features/email-verification/schemas/resend.schema.js
+++ src/features/email-verification/schemas/resend.schema.js
@@ -1,17 +1,5 @@
 const Joi = require('joi');
-const {
-  createUniqueRule,
-} = require('../../../core/validation/rules/unique.rule');
-const { User } = require('../../user/model/user.model');
 
 exports.ResendSchema = {
-  email: Joi.string()
-    .email()
-    .required()
-    .external(
-      createUniqueRule({
-        model: User,
-        field: 'email',
-      }),
-    ),
+  email: Joi.string().email().required(),
 };
