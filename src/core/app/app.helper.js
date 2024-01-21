83816e73ac4e9dc617179fee2880d5540cb8856d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-01-21 08:31:38 +0700

    core: generate client url helper

--- src/core/app/app.helper.js
+++ src/core/app/app.helper.js
@@ -1,3 +1,4 @@
 const { config } = require('./app.config');
 
 exports.generateServerUrl = (path) => `${config.serverUrl}${path}`;
+exports.generateClientUrl = (path) => `${config.clientUrl}${path}`;
