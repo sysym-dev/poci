65aab61eb6e655d77bac301a5cadf7e8dfb2394c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-18 17:27:29 +0700

    feat: validate login

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-const express = require('express');
+import express from 'express';
+import session from 'express-session';
+import flash from 'express-flash-message';
+import { validationResult, matchedData, checkSchema } from 'express-validator';
 
-const server = express();
+const app = express();
 
-server.set('view engine', 'pug');
+app.set('view engine', 'pug');
 
-server.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'test',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+app.use(
+  flash({
+    sessionKeyName: 'flash',
+  }),
+);
+
+app.get('/login', (req, res) => {
   return res.render('login');
 });
+app.post('/login', [
+  checkSchema({
+    email: {
+      isEmail: {
+        errorMessage: 'email invalid',
+      },
+      notEmpty: {
+        errorMessage: 'email required',
+      },
+    },
+    password: {
+      isString: {
+        errorMessage: 'password invalid',
+      },
+      notEmpty: {
+        errorMessage: 'password required',
+      },
+    },
+  }),
+  (req, res) => {
+    const error = validationResult(req);
 
-server.listen(3000, () => {
-  console.log('server listen at 3000');
+    if (!error.isEmpty()) {
+      res.flash('error', error.array()[0].msg);
+
+      return res.redirect('/login');
+    }
+
+    return res.send(matchedData(req));
+  },
+]);
+
+app.listen(3000, () => {
+  console.log('app listen at 3000');
 });
