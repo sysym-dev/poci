8beb65f080eff9b7c9d489c4642bd2a2de8f6444
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-19 18:50:46 +0700

    feat: index page display collection list

---
+++ src/features/index/index.router.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { requireAuth } from '../../middlewares/require-auth.middleware.js';
+import { handleRequest } from '../../middlewares/handle-request.middleware.js';
+import { readCollections } from '../collection/collection.service.js';
+
+const router = Router();
+
+router.route('/').get(
+  requireAuth,
+  handleRequest(async (req, res) => {
+    const collections = await readCollections({
+      userId: req.auth.userId,
+    });
+
+    return res.render('index', { collections });
+  }),
+);
+
+export { router };
