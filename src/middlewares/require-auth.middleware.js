cdc1d695ea36697fdf37e1bccc71606cafb8f35b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-03-10 11:40:34 +0700

    feat: auth check user id

--- src/middlewares/require-auth.middleware.js
+++ src/middlewares/require-auth.middleware.js
@@ -1,5 +1,18 @@
-export function requireAuth(req, res, next) {
+import { pool } from '../core/database/pool.js';
+
+export async function requireAuth(req, res, next) {
   if (!req.session.isLoggedIn) {
+    return res.redirect('/login');
+  }
+
+  const [users] = await pool.execute(`SELECT id FROM users WHERE id = ?`, [
+    req.session.userId,
+  ]);
+
+  if (!users.length) {
+    req.session.isLoggedIn = false;
+    req.userId = null;
+
     return res.redirect('/login');
   }
 
